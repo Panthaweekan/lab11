4a1592c1c9ee4428b8e9d119adf5a44fca60517b
Panthaweekan <tatanoobza1234@hotmail.com>
2021-12-17 00:29:21 +0700

    FINISH lab11_3.cpp

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include <iomanip> 
+
+using namespace std;
+
+int main (){
+	int count = 0;
+    double sum = 0 , mean = 0 , SDmean = 0 , SD = 0;
+    string text;
+    ifstream source("score.txt");
+    while (getline(source,text))
+    {
+        sum += atof(text.c_str());
+        SDmean += pow(atof(text.c_str()),2);
+        count++;
+    }
+
+
+    mean = sum/count;
+    SD = sqrt((SDmean / count) - pow(mean,2));
+
+    cout << setprecision(3);
+	cout << "Number of data = " << count << "\n";
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << SD; 
+    
+    source.close();
+    
+	return 0;
+}
